8c5abb0bdb8bc5267f592a60f5bdee6a28eb319d
Amit Goldberg <amit.goldberg@gmail.com>
2018-10-14 16:18:03 +0100

    Add text subcommand to message subcommand (#1)

--- cmd/message.go
+++ cmd/message.go
@@ -2,36 +2,47 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"github.com/amitizle/telegram-bot-cli/internal/bot"
+	"github.com/logrusorgru/aurora"
 )
 
-var messageCmd = &cobra.Command{
-	Use:   "message",
-	Short: "Send a message to the specified chat ID",
-	Run:   sendMessage,
-}
+var (
+	messageCmd = &cobra.Command{
+		Use:   "message",
+		Short: "Send a message to the specified chat ID",
+	}
+	textCommand = &cobra.Command{
+		Use:   "text",
+		Short: "Send a text message",
+		Run:   sendTextMessage,
+		Args:  cobra.ExactArgs(1),
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(messageCmd)
-	messageCmd.Flags().StringP("message", "m", "", "The message content to send")
-	messageCmd.Flags().Int64P("chatid", "i", 0, "Chat ID to send the message to")
-	messageCmd.Flags().StringP("token", "t", "", "Telegram bot token")
-	viper.BindPFlag("chatid", messageCmd.Flags().Lookup("chatid"))
-	viper.BindPFlag("message", messageCmd.Flags().Lookup("message"))
-	viper.BindPFlag("token", messageCmd.Flags().Lookup("token"))
+	messageCmd.PersistentFlags().Int64P("chatid", "i", 0, "Chat ID to send the message to")
+	messageCmd.PersistentFlags().StringP("token", "t", "", "Telegram bot token")
+	messageCmd.MarkPersistentFlagRequired("chatid")
+	messageCmd.MarkPersistentFlagRequired("token")
+	viper.BindPFlag("chatid", messageCmd.PersistentFlags().Lookup("chatid"))
+	viper.BindPFlag("token", messageCmd.PersistentFlags().Lookup("token"))
+	messageCmd.AddCommand(textCommand)
 }
 
-func sendMessage(cmd *cobra.Command, args []string) {
+func sendTextMessage(cmd *cobra.Command, args []string) {
 	token := viper.GetString("token")
 	chatID := viper.GetInt64("chatid")
-	message := viper.GetString("message")
+	message := args[0] // cobra.ExactArgs(1) is used in the command spec
 	bot, err := bot.New(token)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Println(aurora.Sprintf(aurora.Red("Error while authenticating against Telegram servers: %v\n"), err))
+		os.Exit(1)
 	}
 	bot.Message(chatID, message)
 }
